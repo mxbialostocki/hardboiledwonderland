90a43b7b54d5c4f1f15b20f55ada7504219c8ab0
mxbialostocki <matt.bialostocki@gmail.com>
2020-10-13 16:07:32 +1300

    can map csv to dictionary

--- deconstruct_eb_csv.py
+++ deconstruct_eb_csv.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
-eb_csv = pd.read_csv("./local/ebility_category_test_csv.csv", sep=',', names=None)
+eb_csv = pd.read_csv("./local/ebility_category_test_csv.csv", sep=',', names=None, usecols=['ISBN','TITLE','ON HAND'])
 
-print(eb_csv[0:-1])
-print(eb_csv.head())
 
+# print(eb_csv[["isbn","title","qty sold","cost of sales","nett sales","rrp","on hand","profit"]])
+# print(eb_csv.head())
+
+web_titles = []
+
+print(eb_csv.info())
+
+# for el in eb_csv[3:13]:
+#     print(el)
+
+result = eb_csv.to_dict(orient='records')
+
+print(result)
 # for i in eb_csv:
 #     print(i)
