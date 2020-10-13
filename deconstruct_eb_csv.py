43cf4fd4bba5d8c25f134e76ee720df4f2584edc
mxbialostocki <matt.bialostocki@gmail.com>
2020-10-13 14:58:16 +1300

    can access data

--- deconstruct_eb_csv.py
+++ deconstruct_eb_csv.py
@@ -1,9 +1,9 @@
-import csv
+import pandas as pd
 
-eb_csv = csv.DictReader(open("./local/20200912.csv"))
+eb_csv = pd.read_csv("./local/ebility_category_test_csv.csv", sep=',', names=None)
 
-for row in eb_csv:
-    if row.startswith(!"97"):
-        return
-    else:
-        print(row)
+print(eb_csv[0:-1])
+print(eb_csv.head())
+
+# for i in eb_csv:
+#     print(i)
